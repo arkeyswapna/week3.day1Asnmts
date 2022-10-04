de3691e146518def7b7ac0387a5bce52de26ec6b
arkeyswapna <114006179+arkeyswapna@users.noreply.github.com>
2022-10-04 23:11:37 +0530

    Add files via upload

---
+++ week3Day1Asnmt2/College.java
@@ -0,0 +1,14 @@
+package org.college.week3Day1Asnmt2;
+
+public class College {
+	public void collegeName() {
+		System.out.println("College name is Testleaf");
+	}
+	public void collegeCode() {
+		System.out.println("College code is 1111");
+	}
+	public void collegeRank() {
+		System.out.println("College rank is 2");
+	}
+
+}
