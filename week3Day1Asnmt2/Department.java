3945806a133c9120d4a1063f9bc1e3f956353ff7
arkeyswapna <114006179+arkeyswapna@users.noreply.github.com>
2022-10-04 23:14:34 +0530

    Add files via upload

---
+++ week3Day1Asnmt2/Department.java
@@ -0,0 +1,11 @@
+package org.department.week3Day1Asnmt2;
+
+import org.college.week3Day1Asnmt2.College;
+
+public class Department extends College{
+	public void deptName() {
+		System.out.println("Department name is IT");
+	}
+	
+
+}
