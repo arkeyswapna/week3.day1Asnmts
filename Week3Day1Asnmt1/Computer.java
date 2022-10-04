cc532ad58f302d94da8aa85b102fa457db05dcc9
arkeyswapna <114006179+arkeyswapna@users.noreply.github.com>
2022-10-04 23:18:15 +0530

    Add files via upload

---
+++ Week3Day1Asnmt1/Computer.java
@@ -0,0 +1,9 @@
+package org.system.Week3Day1Asnmt1;
+
+public class Computer {
+ public void computerModel(){
+	 System.out.println("This is Computer class");
+	 
+ }
+
+}
---
+++ Week3Day1Asnmt1/Desktop.java
@@ -0,0 +1,16 @@
+package org.system.Week3Day1Asnmt1;
+
+public class Desktop extends Computer{
+	public void desktopSize() {
+		System.out.println("This is Desktop class");
+	}
+
+	public static void main(String[] args) {
+		Desktop d=new Desktop();
+		d.desktopSize();
+		d.computerModel();
+			
+		}
+		
+	}
+
